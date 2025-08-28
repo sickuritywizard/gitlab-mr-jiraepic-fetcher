7ad02b5689c8e28302981e4f2a9e78d9e8ce83ed
pradeep-kumar-n <pradeep-kumar.n@broadcom.com>
2025-08-28 19:56:32 +0530

    Added -prf for list of pr urls

--- github-pr-commits-downloader.py
+++ github-pr-commits-downloader.py
@@ -13,17 +13,13 @@ import argparse,re
 import base64
 import pdb
 
-MR_OUTPUT_FOLDER = "MR-Download-Results"
-COMMIT_OUTPUT_FOLDER = "Commit-Download-Results"
-
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-pru','--pr-url',dest='pullrequest_url', help="Pull Request URL")
+    parser.add_argument('-prf','--pr-file',dest='pr_file', help="PullRequest File")
     parser.add_argument('-cu','--commit-url',dest='commit_url', help="Commit Request URL")
     parser.add_argument('-cf','--commit-file',dest='commit_file', help="Commit Request File")
-    parser.add_argument('-mu','--merge-url',dest='mr_url', help="Merge Request URL")
-    parser.add_argument('-mf','--merge-file',dest='mr_file', help="Merge Request File")
     parser.add_argument('-t','--token',dest='github_token',help="Github Token (Optional: Fetched from Env: GITHUB_API_TOKEN)")
     parser.add_argument('-ff','--full-file',action="store_true",dest='full_file',help="Download Complete File (Default: Downloads only diff)",default=False)
     args = parser.parse_args()
@@ -31,8 +27,8 @@ def get_args():
     if not args.github_token and not os.getenv("GITHUB_API_TOKEN"):
         exit(colored("[-] Error: Could not fetch Env Variable GITHUB_API_TOKEN. Please set it or provide argument -t <token>","red"))
 
-    if not args.commit_url and not args.commit_file and not args.mr_url and not args.mr_file and not args.pullrequest_url:
-        exit(colored("[-] Error: Either commit_url or commit_file or mr_url or mr_file should be provided","red"))
+    if not args.commit_url and not args.commit_file and not args.pr_file and not args.pullrequest_url:
+        exit(colored("[-] Error: Either commit_url or commit_file or pr_url or pr_file should be provided","red"))
     
 
     return args
@@ -42,12 +38,16 @@ def get_github_api_baseurl(github_url):
     github_api_base_url = f"{parsed_url.scheme}://{parsed_url.netloc}/api/v3"  #https://github.host.com/api/v3
     return github_api_base_url
 
+
+#Example PR URL: https://github.host.com/ORGNAME/REPONAME/pulls/pullnumber
 def download_code_from_pr_url(pr_url):
     global GITHUB_API_TOKEN
 
     #Create Results Folder
     pr_url_dir = pr_url.split("/")
     results_dir = f"{pr_url_dir[4]}_{pr_url_dir[5]}_{pr_url_dir[-1]}"     #org_repo_pullnumber
+
+    parent_dir = os.getcwd()
     try:
         os.mkdir(results_dir)
     except Exception as e:
@@ -79,8 +79,9 @@ def download_code_from_pr_url(pr_url):
         with open(file_name, "w") as fp:
             fp.write(file_content)
 
+
     print(f"[-] Total Files Downloaded: {count}")
-    #Example PR URL: https://github.host.com/ORGNAME/REPONAME/pulls/pullnumber
+    os.chdir(parent_dir)
 
 
 #Fetches all diff as a single response
@@ -95,10 +96,6 @@ def download_diff_from_pr_url(pr_url):
     headers = {"Authorization": f"token {GITHUB_API_TOKEN}", "Accept": "application/vnd.github.v3.diff"}
     response = requests.get(final_api_url, headers=headers)
     print(response.text)
-
-
-def download_code_from_MR(mr_url):
-    pass
 
 
 
@@ -163,17 +160,13 @@ def main():
     if args.pullrequest_url:
         verify_github_token(args.pullrequest_url)
         download_code_from_pr_url(args.pullrequest_url)
-        #Example_Commit_URL = "https://github.host.com/ORGNAME/REPONAME/pulls/pullnumber"
+        #Example_PullRequest_URL = "https://github.host.com/ORGNAME/REPONAME/pulls/pullnumber"
 
     if args.commit_url:
         verify_github_token(args.commit_url)
         download_code_from_commit_url(args.commit_url)
         #Example_Commit_URL = "https://github.host.com/projectname/subproject/-/commit/commithash"
 
-    elif args.mr_url:
-        verify_github_token(args.mr_url)
-        download_code_from_MR(args.mr_url)
-        #Example_MR_URL = "https://github.host.com/projectname/subproject/-/merge_requests/177"
 
     elif args.commit_file:
         with open(os.path.join(curr_dir, args.commit_file), "r") as fileptr:
@@ -184,20 +177,13 @@ def main():
                 print(colored(f"\n[-] {commit_url}","cyan"))
                 download_code_from_commit_url(commit_url)
 
-    elif args.mr_file:
-        with open(os.path.join(curr_dir, args.mr_file), "r") as fileptr:
+    elif args.pr_file:
+        with open(os.path.join(curr_dir, args.pr_file), "r") as fileptr:
             urls = fileptr.readlines()
             verify_github_token(urls[0].strip())
             for merge_url in urls:
                 merge_url = merge_url.strip()
                 print(colored(f"\n[-] {merge_url}","cyan"))
-                download_code_from_MR(merge_url)
+                download_code_from_pr_url(merge_url)
 
 main()
-
-
-'''
-#TODO
-1)Add support for full file download even in MergeRequests
-2)Test with multiple Commit requests, as last time it was given empty response for few files
-'''
